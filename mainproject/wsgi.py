d617fb926392e543cd73711d37afb78934b3260b
krvinayak28 <krvinayak28@gmail.com>
2020-09-12 18:28:51 +0530

    Added congig Vars

--- mainproject/wsgi.py
+++ mainproject/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainproject.settings')
@@ -16,5 +16,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainproject.settings')
 application = get_wsgi_application()
 
 
-from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
