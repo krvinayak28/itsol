c42b4eb0adab71a3127e0d7ccae152f8a41467ed
krvinayak28 <krvinayak28@gmail.com>
2020-09-12 18:26:24 +0530

    Added congig Vars

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -141,7 +141,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
