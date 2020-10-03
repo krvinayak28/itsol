e8a4c9a8c35d9b3e7f5dc44c531d2eade40f4918
krvinayak28 <krvinayak28@gmail.com>
2020-10-03 13:10:25 +0530

    brother-it to encodernapps

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['encodernapps.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
